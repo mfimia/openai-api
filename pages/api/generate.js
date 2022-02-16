f3d20cc789c116be7cd4d1540886de4ba5d079e7
Martin Fimia <martin.fimia.osuna@hotmail.com>
2022-02-16 12:15:08 +0100

    first commit

---
+++ pages/api/generate.js
@@ -0,0 +1,31 @@
+import dotenv from "dotenv";
+import { Configuration, OpenAIApi } from "openai";
+
+dotenv.config();
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+export default async function (req, res) {
+  const completion = await openai.createCompletion("text-davinci-001", {
+    prompt: generatePrompt(req.body.animal),
+    temperature: 0.6,
+  });
+  res.status(200).json({ result: completion.data.choices[0].text });
+}
+
+function generatePrompt(animal) {
+  const capitalizedAnimal =
+    animal[0].toUpperCase() + animal.slice(1).toLowerCase();
+  // give the instructions below
+  return `Suggest three names for an animal that is a superhero.
+
+  Animal: Cat
+  Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
+  Animal: Dog
+  Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
+  Animal: ${capitalizedAnimal}
+  Names:`;
+}
